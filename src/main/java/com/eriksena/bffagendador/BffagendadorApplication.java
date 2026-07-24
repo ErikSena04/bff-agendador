50cdec019fcf1fc2fea88b6a237a82068f03c832
Erik <eriksena0104@outlook.com>
2026-07-24 20:45:10 -0300

    feat:esqueleto do projeto

---
+++ src/main/java/com/eriksena/bffagendador/BffagendadorApplication.java
@@ -0,0 +1,13 @@
+package com.eriksena.bffagendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffagendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffagendadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eriksena/bffagendador/BffagendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eriksena.bffagendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffagendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
